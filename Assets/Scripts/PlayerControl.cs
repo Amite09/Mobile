c8695d922a8dd083914652496cf12479f7fb89a5
AmitE09 <amitelia93@gmail.com>
2020-05-02 12:08:14 +0300

    add menu and improved shooting and rotataing

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,16 +4,6 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-
-    private SwipeInput input;
-
-    private Time timeTouched;
-    private Time timeLeft;
-    private float startTouch;
-
-    public bool swipingRight = false;
-    public bool swipingLeft = false;
-
     public GameObject[] projs;
     public Vector3 projSpawnPos;
 
@@ -22,87 +12,52 @@ public class PlayerControl : MonoBehaviour
 
     private int j = 0;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        input = transform.root.GetComponent<SwipeInput>();
-    }
 
     // Update is called once per frame
     void Update()
     {
-        checkIfMoved();    
-        Shoot();
+        checkTouches();
     }
 
-
-    void checkIfMoved(){
-        if(!(swipingLeft || swipingRight)){
-            if(input.swipedLeft){       
-                swipingLeft = true;
-                currentState = (currentState == 3 ? currentState - 3 : currentState + 1);
-
-            } else if(input.swipedRight){
-                swipingRight = true;
-                currentState = (currentState == 0 ? currentState + 3 : currentState - 1);
-            }
-        }
-
-        if(swipingLeft){
-            if(j < 18){
-                j++;
-                transform.Rotate(0, 0, 5);
-            } else {
-                j = 0;
-                swipingLeft = false;
-            }
-        } else if(swipingRight) {
-            if(j < 18){
-                j++;
-                transform.Rotate(0, 0, -5);
-            } else {
-                j = 0;
-                swipingRight = false;
-            }
-        }
-    }
-
-
-    void Shoot(){
-
-
-        if(swipingLeft || swipingRight){
-            return;
-        }
-
+    void checkTouches(){
         foreach (Touch touch in Input.touches){
             if (touch.fingerId == 0){
-                
-                if (Input.GetTouch(0).phase == TouchPhase.Began){
-                    startTouch = Time.time;
-                }
-                if (Input.GetTouch(0).phase == TouchPhase.Ended && Time.time - startTouch < 0.1f){
-                    Vector3 _touchPosition = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, 5);
+                Vector3 _touchPosition = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, 5);
+                Vector3 worldtouchPos = Camera.main.ScreenToWorldPoint(_touchPosition);
+
+                if(worldtouchPos.x >= 0 && worldtouchPos.y < 0.5 && j == 0){
+                    currentState = (currentState == 0 ? currentState + 3 : currentState - 1);                   
+                    StartCoroutine(rotatePlayer("Left"));
+                } else if(worldtouchPos.x < 0 && worldtouchPos.y < 0.5 && j == 0){
+                    currentState = (currentState == 3 ? currentState - 3 : currentState + 1);
+                    StartCoroutine(rotatePlayer("Right"));
+                } else if(worldtouchPos.y > 0.5 && touch.phase == TouchPhase.Began) {
                     GameObject proj = Instantiate(projs[currentState], projSpawnPos, Quaternion.identity);
-                    Vector3 worldtouchPos = Camera.main.ScreenToWorldPoint(_touchPosition);
                     worldtouchPos.x *= 10;
                     worldtouchPos.y *= 10;
                     proj.GetComponent<Projectiles>().Target = worldtouchPos;
                     proj.GetComponent<Projectiles>().Color = states[currentState];
                 }
-
             }
         }
-
-        // if(Input.GetMouseButtonDown(0)){
-        //     Vector3 _mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5);
-        //     if (_mousePosition.y < 190){
-        //         return;
-        //     }
-        //     GameObject proj = Instantiate(projs[currentState], projSpawnPos, Quaternion.identity);
-        //     Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(_mousePosition);
-        //     proj.GetComponent<Projectiles>().Target = worldMousePos;
-        //     proj.GetComponent<Projectiles>().Color = states[currentState];
-        // }
     }
+
+    IEnumerator rotatePlayer(string direction){
+        if (direction == "Right"){
+            while (j < 18){
+                transform.Rotate(0, 0, 5);
+                j++;
+                yield return new WaitForEndOfFrame();
+            }
+        } else if (direction == "Left"){
+            while (j < 18){
+                transform.Rotate(0, 0, -5);
+                j++;
+                yield return new WaitForEndOfFrame();
+            }
+        }
+        j = 0;
+    }
+
+
 }
---
+++ Assets/UI/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class Menu : MonoBehaviour
+{
+
+    public void PlayGame(){
+
+        SceneManager.LoadScene("Game");
+
+    }
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
